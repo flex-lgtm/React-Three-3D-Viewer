7259c01b92efe796f90fbcc0e3baae6471ea5020
flex-lgtm <flexxlgtm@gmail.com>
2024-06-03 17:26:05 +0900

    Update model and lights

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,46 @@
+// import * as THREE from 'three';
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei';
 import './App.css'
 
-function Model() {
-  // const group = useRef();
-  const { scene } = useGLTF('704_01.glb'); // Replace with your GLB model path
+// THREE.ColorManagement.legacyMode = false;
 
-  // Set up rotating animation
-  // useFrame(() => {
-  //   group.current.rotation.y += 0.01; // Adjust rotation speed as needed
-  // });
+function Model({ modelUrl }) {
+  // const group = useRef();
+  const { scene } = useGLTF(modelUrl);
+  // const texture = useLoader(THREE.TextureLoader, textureUrl);
+
+  // useEffect(() => {
+  //   texture.encoding = THREE.sRGBEncoding;
+    
+  //   Object.values(materials).forEach((material) => {
+  //     if (material.map) {
+  //       material.map = texture;
+  //       material.map.encoding = THREE.sRGBEncoding;
+  //       material.color = new THREE.Color(materialColor);
+  //       material.needsUpdate = true;
+  //     }
+  //   });
+  // }, [materials, texture, materialColor]);
 
   return <primitive object={scene} />;
 }
 
-// function Scene() {
-//   const colorMap = useLoader(TextureLoader, '704_01-.webp');
-//   return (
-//     <>
-//       <ambientLight intensity={0.2} />
-//       <directionalLight />
-//       <mesh>
-//         <sphereGeometry args={[1, 32, 32]} />
-//         <meshStandardMaterial map={colorMap} />
-//       </mesh>
-//     </>
-//   )
-// }
-
 function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 0.4], fov: 45 }}>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]}/>
-      <directionalLight  color={0xffffff} intensity={Math.PI * 1.5} castShadow/>
+    <Canvas camera={{ position: [0.8, 0, 0.1], fov: 25 }}>
+      <ambientLight  />
+      <pointLight position={[10, 10, 10]} />
+      {/* <spotLight /> */}
+      {/* <hemisphereLight skyColor={0xffffff} groundColor={0x444444} intensity={0.6} position={[0, 20, 0]} /> */}
+      <directionalLight intensity={Math.PI * 1} position={[10, 30, 5]} />
+      <directionalLight intensity={Math.PI * 0.5} position={[-10, 5, 5]} />
+      <directionalLight intensity={Math.PI * 1} position={[10, -25, 10]} />
+      <directionalLight intensity={Math.PI * 1} position={[0, -10, 10]} />
+      <directionalLight intensity={Math.PI * 0.5} position={[10, 25, -10]} />
       <Suspense fallback={null}>
-        <Model />
+        <Model modelUrl='scene.gltf' />
       </Suspense>
       <OrbitControls />
     </Canvas>
@@ -44,3 +48,6 @@ function App() {
 }
 
 export default App;
+
+
+
